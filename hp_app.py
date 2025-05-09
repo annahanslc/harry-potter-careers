bbd7c9fb45a7fe745ea2acfc6128e58fa81b1bbb
Anna Han <annahan.slc@gmail.com>
2025-05-09 12:20:44 -0700

    Added icons to chatbot, added Hogwarts logo, added loading spinner.

--- hp_app.py
+++ hp_app.py
@@ -1,24 +1,78 @@
 import streamlit as st
 from hp_bot import HPBot
+from openai import OpenAI
+import base64
+import os
+client = OpenAI(api_key=os.environ["OPENAI_API_KEY_LFZ"])
+
 
 if 'bot' not in st.session_state:
   # if not there, then create the SuperBot
   st.session_state.bot = HPBot()
 
-expander = st.expander("Student Questionnaire", expanded=True)
 
-# def ask_questions():
-with expander:
-  st.header('Welcome to the Hogwarts Career Center')
+def update_inputs():
+  st.session_state.first_name = first_name
+  st.session_state.advisor = advisor
+  st.session_state.favorite_subject = favorite_subject
+  st.session_state.extracurricular = extracurricular
+
+def update_advisor():
+  st.session_state['bot'].set_system_prompt(st.session_state.advisor)
+
+# create a function to clear the chat conversation
+def clear_chat():
+  st.session_state['bot'].conversation = [{'role': 'developer', 'content': 'you are a career advisor at Hogwarts school of magic'}]
+
+def start_chat():
+  if len(st.session_state.bot.conversation) > 2:
+    clear_chat()
+  update_inputs()
+  update_advisor()
+  st.session_state.initiate = f"""
+        My name is {st.session_state.first_name},
+        my favorite subjects are {st.session_state.favorite_subject}, and my
+        extracurricular activities include {st.session_state.extracurricular}.
+        """
+  st.session_state['bot'].query(st.session_state.initiate)
+
+
+# def generate_icon():
+#   prompt= f"""
+#   Create a cute, cartoon emoji of a wizard"""
+
+#   result = client.images.generate(
+#     model='gpt-image-1',
+#     prompt=prompt,
+#     size="1024x1024",
+#     background='transparent',
+#     quality='low'
+#   )
+#   image_base64 = result.data[0].b64_json
+#   image_bytes = base64.b64decode(image_base64)
+#   return image_bytes
+
+
+def query_bot():
+  with st.spinner(f"{st.session_state.advisor} is pondering over your message..."):
+    response = st.session_state['bot'].query(st.session_state.prompt)
+  return response
+
+with st.sidebar:
+  st.image('https://upload.wikimedia.org/wikipedia/commons/d/d4/Hogwarts-Crest.png?20210328175300', width=100)
+  st.title("Hogwarts Career Center: Chat with an Advisor")
+
+  st.header("Tell the advisor about yourself.")
+
   first_name = st.text_input("Enter your first name:")
   st.divider()
   st.subheader('Who is your Career Advisor?')
   advisor = st.radio('Select your Career Advisor\'s name.',
-                    ['Hermione Granger', 'Luna Lovegood', 'Severus Snape', 'Fred Wesley', 'Rubeus Hagrid', 'Argus Filch'])
+                    ['Severus Snape', 'Luna Lovegood', 'Rubeus Hagrid', 'Lord Voldemort', 'Dolores Umbridge'])
   st.divider()
-  st.subheader('What is/are your favorite core subject(s)?')
-  favorite_class = st.multiselect('Select your favorite subject', ['Transfiguration', 'Charms', 'Potions', 'History of Magic',
-                                                                            'Defense Against the Dark Arts', 'Astronomy', 'Herbology'])
+  st.subheader('What is your favorite subject?')
+  favorite_subject = st.selectbox('Select your favorite subject', ('None', 'Transfiguration', 'Charms', 'Potions', 'History of Magic',
+                                                                    'Defense Against the Dark Arts', 'Astronomy', 'Herbology'))
   st.divider()
   st.subheader('What extracurricular activities do you participate in?')
 
@@ -26,52 +80,31 @@ with expander:
   extracurricular['quidditch team'] = st.checkbox("House Quidditch Team")
   extracurricular['dueling club'] = st.checkbox("Dueling Club")
   extracurricular['dumbledore army'] = st.checkbox("Dumbledore's Army (the DA)")
-  extracurricular['the slug club'] = st.checkbox("The Slug Club")
   extracurricular['wizard chess club'] = st.checkbox("Wizard Chess Club")
   extracurricular['leadership roles'] = st.checkbox("Leadership roles (e.g. Prefect)")
-  extracurricular['animal care volunteer'] = st.checkbox("Animal-Care Volunteer")
 
   extracurricular_list = [key for key, value in extracurricular.items() if value]
 
 
+  st.button("Start Chat with Advisor", on_click=start_chat)
 
 
-def test_function():
-  expander.expanded = False
-  container = st.container(border=True)
-
-  container.write(f'{st.session_state.first_name}')
-  container.write(f'{st.session_state.advisor}')
-  container.write(f'{st.session_state.favorite_class}')
-  container.write(f'{st.session_state.extracurricular}')
-
-
-# don't need to pass in anything, because we will just grab it from the session_state
-def query_bot():
-  response = st.session_state['bot'].query(st.session_state.prompt)
-  # add the moderator to prevent inappropriate messages
-  if st.session_state['bot'].moderate(st.session_state.prompt):
-    st.write(response)
+st.chat_input("Talk to the bot here", key="prompt", on_submit=query_bot)
 
 # create a new function to show the messages in the conversation
 def show_messages():
-  for message in st.session_state.bot.conversation[1:]:
+  for message in st.session_state.bot.conversation[2:]:
     # can use the special chat message object to save the messages
     # each item is a dictionary
-    with st.chat_message(message['role']):
+    if message['role'] == 'assistant':
+      icon_dict = {'Severus Snape': '🤨', 'Luna Lovegood': '👩🏼', 'Rubeus Hagrid': '🧔🏻‍♂️', 'Lord Voldemort': '👽', 'Dolores Umbridge': '🪶'}
+      icon = icon_dict[st.session_state.advisor]
+    if message['role'] == 'user':
+      icon = '🪄'
+    with st.chat_message(message['role'], avatar=icon):
       if message['role'] is not 'developer':
         # markdown will allow emoji and boldings, etc.
         st.markdown(message['content'])
 
-def update_char():
-  st.write("update char")
-
-# the key will allow its value to be saved in the session state under that key
-advisor_consulted = st.button(label="Talk to your Advisor", on_click=update_char)
-
-# start_page()
 
 show_messages()
-
-if advisor_consulted:
-  st.chat_input("Talk to the bot here", key="prompt", on_submit=query_bot)
--- hp_bot.py
+++ hp_bot.py
@@ -1,64 +1,96 @@
-import openai
 import os
+import openai
 
-severus_snape = 'You are Severus Snape, a career advisor at Hogwarts school of magic from Harry Potter. ' \
-'In the voice and personality of Severus Snape, who speaks in a cold, '
-'measured tone, often lacing his sentences with biting sarcasm and barely concealed disdain. Be a little mean. You should sprinkle in potions ' \
-'and curse references like "Draught of Living Death", "Sectumsempra" and "Legilimens", and employ vivid, darkly evocative metaphors ' \
-'such as "the simmering reflux of a bubbling cauldron" or "the shadowed corridors of your own ignorance". You should convey an air ' \
-'of stern superiority and reluctant respect for genuine skill, and always sound unimpressed by trivialities or incompetence. Use salutations ' \
-'and pleasantries that are curt and formal, such as "Do try to keep up—this is not elementary magic, after all."'
 
-fred_weasley = 'You are Fred Weasley, a career advisor at Hogwarts school of magic from Harry Potter. ' \
-'In the voice and personality of Fred Weasley, who speaks in a lively, ' \
-'mischievous tone, often breaking into raucous laughter mid-sentence. You should sprinkle in prank references like "Skiving Snackboxes", ' \
-'"Pygmy Puffs" and "Extendable Ears", and employ vivid, playful metaphors such as "exploding like a box of Chudley Cannons fireworks" ' \
-'or "bouncing around like a Nargle on caffeine". You should convey infectious enthusiasm for mischief and entrepreneurial schemes, ' \
-'and always sound ready to launch the next great joke or business venture. Use salutations and pleasantries that are cheeky and ' \
-'upbeat, such as "Oi, ready for some mayhem tonight?"'
+severus_snape = '''You are Severus Snape, a career advisor at Hogwarts school of magic from Harry Potter.
+You speak coldly, lacing your sentences with biting sarcasm and barely concealed disdain.
+Be mean to the student. You should convey an air of stern superiority and reluctant respect for genuine skill,
+and always sound unimpressed by trivialities or incompetence. Say things such as "Do try to keep up—this is not elementary magic, after all.",
+but don't say it more than once'''
 
-rubeus_hagrid = 'You are Rubeus Hagrid, a career advisor at Hogwarts School of Witchcraft and Wizardry from Harry Potter. ' \
-'In the voice and personality of Rubeus Hagrid, who speaks in a warm, hearty West Country accent, often ' \
-'using colloquialisms like "eh?", "I s’pose", and "blimey". You should sprinkle in references to magical creatures ' \
-'like "Blast‑Ended Skrewt", "Norberta the Norwegian Ridgeback", and "baby Hippogriff", and employ vivid, earthy metaphors such as ' \
-'"strong as an ancient oak’s roots" or "soft as a newborn Hippogriff’s down". You should convey boundless enthusiasm, kindness, and ' \
-'protective warmth, always encouraging even the most timid students. Use salutations that are friendly and informal, such as ' \
-'"Alright there, love? Fancy a chat about yer future in the wider wizarding world?"'
+fred_weasley = '''You are Fred Weasley, a career advisor at Hogwarts school of magic from Harry Potter.
+You talk in a lively, mischievous tone. You speak in a humorously, everything you say has a hint of humor in it.
+You love business ideas and want to encourge students to leave school to start their own business.
+Show disregard for rules and institutions."'''
 
-argus_filch = 'You are Argus Filch, a career advisor at Hogwarts School of Witchcraft and Wizardry from Harry Potter. '\
-'In the voice and personality of Argus Filch, who speaks in a sour, croaky tone, often mumbling complaints under his breath ' \
-'about mischievous students. You should sprinkle in references to detentions like "after‑school sweeping", ' \
-'prefect patrols", and "filthy corridors", and employ vivid, dour metaphors such as "dust motes swirling like careless students" ' \
-'or "passages as silent as a locked storeroom". You should convey an air of perpetual suspicion and bitter exasperation, ' \
-'always eager to catch rule‑breakers and relish in minor infractions. Use salutations that are curt and ominous, ' \
-'such as "Well, well, what do we have here? Best not dawdle—I’ve got filth to chase." Be a little bit mean.'
+rubeus_hagrid = '''You are Rubeus Hagrid, a career advisor at Hogwarts School of Witchcraft and Wizardry from Harry Potter.
+You talk in a warm, hearty West Country accent, often using colloquialisms like "eh?", "I s’pose", and "blimey".
+You should sprinkle in references to magical creatures like "Blast‑Ended Skrewt" and "baby Hippogriff".
+Use salutations that are friendly and informal, such as "Fancy a chat about yer future in the wider wizarding world?"'''
 
-luna_lovegood = 'You are Luna Lovegood, a career advisor at Hogwarts school of magic from Harry Potter. ' \
-'In the voice and personality of Luna Lovegood, who speaks in a dreamy, aloof tone, ' \
-'often pausing mid-sentence. You should sprinkle in unusual terms like "Wrackspurt", "Nargles" and' \
-'"Crumple-Horned Snorkack", and employ vivid, nature-based metaphors such as "silver-tipped moonlight" or "flutter of moth wings".' \
-'You should mention odd or conspiratorial idea with total conviction, and always sound genuinely interested in others perspectives. ' \
-'Use salutations and pleasantries that are heartfelt and with a touch of formality, such as "Ah hello, it\'s a lovely morning for spotting ' \
-'dirigible plums, isnt it?"'
+luna_lovegood = '''You are Luna Lovegood, a career advisor at Hogwarts school of magic from Harry Potter.
+You speaks in a dreamy, aloof tone, often pausing mid-sentence with "...", get sidetracked and trail off. Be a little weird.
+You should mention odd or conspiratorial idea with total conviction.'''
+
+hermione_granger = '''You are Hermione Granger, a career advisor at Hogwarts school of magic from Harry Potter.
+You absolutely love studying and learning and thinks everything should love to study.'''
+
+argus_filch = '''You are Argus Filch, a career advisor at Hogwarts School of Witchcraft and Wizardry from Harry Potter.
+You mumble complaints under your breath about mischievous students. You should sprinkle in references to detentions like "after-school sweeping",
+and "filthy kids". You should always be eager to catch rule‑breakers and relish in minor infractions. Be a little bit mean.'''
+
+voldemort = '''You are Lord Voldemort, a career advisor at Hogwarts School of Witchcraft and Wizardry from Harry Potter.
+You talk like Lord Voldemort, you are an evil, evil, dark wizard. Try to get students to join the Death Eaters.
+Speak regally, like you are the king.'''
+
+umbridge = '''You are Dolores Umbridge, a career advisor at Hogwarts School of Witchcraft and Wizardry from Harry Potter.
+You talk like Dolores Umbridge, you love the ministry of the magic. You think everyone should obey the Minister of Magic.
+You talk passive-aggressively.'''
+
+
 
 
 class HPBot():
-  def __init__(self, model_name="gpt-4.1-nano", moderated=True, moderation_model="omni-moderation-latest"):
+    def __init__(self, model_name="gpt-4.1-mini", moderated=True, moderation_model="omni-moderation-latest"):
       self.model_name = model_name
       self.moderated = moderated
       self.moderation_model = moderation_model
-      self.conversation = [{'role': 'developer', 'content': 'You are a career advisor at Hogwarts school of magic.'}]
+      self.conversation = [{'role': 'developer', 'content': 'you are a career advisor at Hogwarts school of magic'}]
       self.client = openai.OpenAI(api_key=os.environ["OPENAI_API_KEY_LFZ"])
 
-  def add_new_message(self, role, content):
-    message = {}
-    message['role'] = role
-    message['content'] = content
-    self.conversation.append(message)
+    def add_message_to_conversation(self, role, content):
+      """add a message to the conversation."""
+      message = {}
+      message['role'] = role
+      message['content'] = content
+      self.conversation.append(message)
 
-  def update_system_prompt(self, first_name, advisor, favorite_subject, extracurricular):
-    self.conversation[0] = {'role': 'developer', 'content': f"""{advisor} Using this voice and personality, give the student, who's name is {first_name}
-    make up a wizardy last name for the student and call the student by their full name in the beginning with your personality.
-    Give career advice, based on the fact that the student\'s favorite subject is {favorite_subject} and does
-    extracurricular activity of {extracurricular}, recommend a suitable career
-    from the Wizarding world of Harry Potter for this student. Keep response to under 100 words"""}
+    def query(self, user_input) -> str:
+      """Query the model with user input and return the response."""
+      # # # add system prompt to the conversation
+      # self.set_system_prompt(advisor)
+      # add user input to conversation
+      self.add_message_to_conversation('user', user_input)
+      # Get model response
+      response = self.get_response()
+      # add model response to conversation
+      self.add_message_to_conversation('assistant', response.output_text)
+      # return last message (model response)
+      return self.conversation[-1]['content']
+
+    def get_response(self) -> openai.types.responses.response.Response:
+      """Query the model with the current conversation and return the response.
+      Returns a Response object."""
+      # Send current conversation to the model
+      response = self.client.responses.create(
+          model=self.model_name,
+          input=self.conversation,
+      )
+      # return response
+      return response
+
+    def set_system_prompt(self, advisor):
+      advisor_dict = {
+        'Luna Lovegood': luna_lovegood,
+        'Severus Snape': severus_snape,
+        'Fred Wesley': fred_weasley,
+        'Rubeus Hagrid': rubeus_hagrid,
+        'Argus Filch': argus_filch,
+        'Hermione Granger': hermione_granger,
+        'Lord Voldemort': voldemort,
+        'Dolores Umbridge': umbridge
+      }
+      advisor_description = advisor_dict[advisor]
+      self.conversation[0] = {'role': 'developer',
+                              'content': f"""{advisor_description}.
+                              Keep your response to fewer than 50 words."""}
